bd29d199b0d9c56da93b6b0281c1c8c0a105c0d1
Jan Tomasek <jan@tomasek.cz>
2020-09-21 16:00:51 +0200

    Remove unnecesary space.

--- ldapsonly.c
+++ ldapsonly.c
@@ -19,11 +19,11 @@ int ldapsonly_bind(Slapi_PBlock *pb) {
   PRNetAddr clientAddress;
   char clientAddressString[LDAPSONLY_MAX_ADDRESS];
   char **argv;
-  int  argc; 
+  int  argc;
   int  i;
   int  permit = 0;
   char *permited_by;
-  
+
   /* Log a message to the server error log. */
   slapi_log_error( SLAPI_LOG_PLUGIN, "ldapsonly_bind", "Pre-operation bind function called.\n" );
 
@@ -47,7 +47,7 @@ int ldapsonly_bind(Slapi_PBlock *pb) {
       slapi_send_ldap_result(pb, LDAP_OPERATIONS_ERROR,
 			     NULL, NULL, 0, NULL);
       return(1);
-    } 
+    }
 
     PR_NetAddrToString(&clientAddress, clientAddressString, LDAPSONLY_MAX_ADDRESS);
 
@@ -67,7 +67,7 @@ int ldapsonly_bind(Slapi_PBlock *pb) {
 		      "User '%s' from IP '%s' is not using SSL(%d). Bind is permitted because he comes from configured IP '%s'.\n",
 		      dn, clientAddressString, isSSL, permited_by);
     } else {
-      slapi_log_error(SLAPI_LOG_FATAL, "ldapsonly_bind", 
+      slapi_log_error(SLAPI_LOG_FATAL, "ldapsonly_bind",
 		      "User '%s' from IP '%s' is not using SSL(%d). Bind refused.\n",
 		      dn, clientAddressString, isSSL);
       slapi_send_ldap_result(pb, LDAP_STRONG_AUTH_REQUIRED,
@@ -75,7 +75,7 @@ int ldapsonly_bind(Slapi_PBlock *pb) {
       return(1);
     };
   };
-  
+
   return(0);
 }
 
@@ -85,13 +85,15 @@ int ldapsonly_init(Slapi_PBlock *pb) {
   slapi_pblock_get (pb, SLAPI_PLUGIN_IDENTITY, &my_plugin_identity);
 
   /* Register the pre-operation bind function and specify
-  the server plug-in version. */
+     the server plug-in version. */
   if (slapi_pblock_set( pb, SLAPI_PLUGIN_VERSION,SLAPI_PLUGIN_VERSION_03 )!=0 ||
       slapi_pblock_set( pb, SLAPI_PLUGIN_DESCRIPTION,(void *)&bindpdesc )!=0 ||
-      slapi_pblock_set( pb, SLAPI_PLUGIN_PRE_BIND_FN,(void *) ldapsonly_bind )!=0 ) {
+      slapi_pblock_set( pb, SLAPI_PLUGIN_PRE_BIND_FN,(void *)ldapsonly_bind )!=0 ) {
     slapi_log_error( SLAPI_LOG_PLUGIN, "ldapsonly_init" , "Failed to set version and function.\n" );
     return(-1);
   }
+
+  slapi_log_error( SLAPI_LOG_PLUGIN, "ldapsonly_init" , "Initialized.\n" );
 
   return(0);
 }
